36779369539cb41487a3ab8a1490c91357713e3f
becassin <winni_the_poo@hotmail.com>
2014-06-25 21:25:43 +0200

    update moderniser

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         concat: {
             dist: {
                 src: [
-                    'js/libs/modernizr-2.6.2-respond-1.1.0.min.js',
+                    'js/libs/modernizr-2.8.2-respond-1.1.0.min.js',
                     'js/script.js',
                     'js/plugins.js'
                 ],
